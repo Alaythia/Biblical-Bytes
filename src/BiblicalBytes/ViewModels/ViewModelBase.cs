f550ff483b3cedd4f97794ea264ad11b4150050f
Gregory Jordan <greg@gregandlisajordan.com>
2024-06-22 22:15:22 -0500

    Refactor and update project components
    - Updated namespace in `App.axaml.cs` and simplified `App` class.
    - Removed `Dependencies` section from README.
    - Updated Avalonia package to `11.0.11` in `BiblicalBytes.csproj`, added `Autofac` and `Serilog` packages.
    - Simplified `ViewModelBase` class by removing unnecessary nesting.
    - Updated application icon and title in `MainWindow.axaml`.
    - Simplified `MainWindow` class by removing unnecessary nesting.
    - Added `Serilog.Enrichers.Memory.dll` binary file with metadata.
    - General improvements to the application's visual representation and compatibility.

--- src/BiblicalBytes/ViewModels/ViewModelBase.cs
+++ src/BiblicalBytes/ViewModels/ViewModelBase.cs
@@ -1,8 +1,7 @@
 ﻿using ReactiveUI;
 
-namespace BiblicalBytes.ViewModels
+namespace BiblicalBytes.ViewModels;
+
+public class ViewModelBase : ReactiveObject
 {
-    public class ViewModelBase : ReactiveObject
-    {
-    }
 }
--- src/BiblicalBytes/Views/MainWindow.axaml.cs
+++ src/BiblicalBytes/Views/MainWindow.axaml.cs
@@ -1,12 +1,11 @@
 using Avalonia.Controls;
 
-namespace BiblicalBytes.Views
+namespace BiblicalBytes.Views;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
